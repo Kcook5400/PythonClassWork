5b7a8352be4829fc1e03d8fcfd5246acff079e17
Kcook5400 <kcook@dmacc.edu>
2020-09-06 19:30:26 -0500

    Initial Version

---
+++ Module2/hello_world_function.py
@@ -0,0 +1,14 @@
+"""
+PEP: 8
+Title: My function
+Author: Kevin Cook
+Status: Active
+Type: Process
+Created: 06-Septmber-2020
+Post: 06-September-2020
+History:
+"""
+def hello_world_function():
+    print("Hello World!")
+
+hello_world_function()
