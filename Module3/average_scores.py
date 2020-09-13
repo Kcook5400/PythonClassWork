112e61bf77a6d638715966d9ec4b2c04cee229fc
Kcook5400 <kcook@dmacc.edu>
2020-09-13 15:28:37 -0500

    Module 3 Average Scores

--- Module3/average_scores.py
+++ Module3/average_scores.py
@@ -5,5 +5,19 @@
 
 
 def average():
-    pass
+    score1 = int(input("Enter 1st Score"))
+    score2 = int(input("Enter 2nd Score"))
+    score3 = int(input("Enter 3rd Score"))
+    average =  (score1+score2+score3)/3
+    str(average)
+    return  average
 
+if __name__ == '__main__':
+#program to run
+    name_first = str(input("Enter your first name"))
+    name_last = str(input("Enter your last name"))
+    age = str(input("Enter your age"))
+    average_score = average()
+    print("First Name:",name_first)
+    print("Last Name:",name_last)
+    print("Your average score is: ",average_score)
--- Module3/test_average_scores.py
+++ Module3/test_average_scores.py
@@ -4,8 +4,10 @@ from average_scores import average
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(average(1,3,8),4)
 
 
 if __name__ == '__main__':
+
     unittest.main()
+
