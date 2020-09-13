05c32c93d0d99a2efe4522307e5c6a535499ba5e
Kcook5400 <kcook@dmacc.edu>
2020-09-13 15:39:58 -0500

    Module 3 Average Scores

--- Module3/average_scores.py
+++ Module3/average_scores.py
@@ -1,23 +1,29 @@
-
-
-
-
+"""
+Title: average_scores
+Author: Kevin Cook
+Status: Active
+Type: Process
+Created: 13-September-2020
+Post: 13-September-2020
+History:
+"""
 
 
 def average():
     score1 = int(input("Enter 1st Score"))
     score2 = int(input("Enter 2nd Score"))
     score3 = int(input("Enter 3rd Score"))
-    average =  (score1+score2+score3)/3
+    average = (score1 + score2 + score3) / 3
     str(average)
-    return  average
+    return average
+
 
 if __name__ == '__main__':
-#program to run
+    # program to run
     name_first = str(input("Enter your first name"))
     name_last = str(input("Enter your last name"))
     age = str(input("Enter your age"))
     average_score = average()
-    print("First Name:",name_first)
-    print("Last Name:",name_last)
-    print("Your average score is: ",average_score)
+    print("First Name:", name_first)
+    print("Last Name:", name_last)
+    print("Your average score is: ", average_score)
--- Module3/test_average_scores.py
+++ Module3/test_average_scores.py
@@ -1,10 +1,9 @@
 import unittest
-
 from average_scores import average
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(average(1,3,8),4)
+        assert average_scores.average() == 3
 
 
 if __name__ == '__main__':
