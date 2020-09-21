ac6f04ce7288c11e71b645783b65148fea8168ee
Kcook5400 <kcook@dmacc.edu>
2020-09-20 19:53:15 -0500

    Module 4 Testing Scenarios

--- Module 4/test.py
+++ Module 4/test.py
@@ -43,5 +43,47 @@ class TestSetTwo(unittest.TestCase):
     def test_price_under_30_10_cash_20_discount(self):
             self.assertEquals(calculate_price(11, 5, 20), 18.65)
 
+class TestSetThree(unittest.TestCase):
+    def test_pass(self):
+        pass
+    def test_price_under_50_5_cash_10_discount(self):
+        self.assertEquals(calculate_price(31, 5, 10), 18.65)
+
+    def test_price_under_50_5_cash_15_discount(self):
+        self.assertEquals(calculate_price(31, 5, 15), 18.65)
+
+    def test_price_under_50_5_cash_20_discount(self):
+        self.assertEquals(calculate_price(31, 5, 20), 18.65)
+
+    def test_price_under_50_10_cash_10_discount(self):
+            self.assertEquals(calculate_price(31, 5, 10), 18.65)
+
+    def test_price_under_50_10_cash_15_discount(self):
+            self.assertEquals(calculate_price(31, 5, 15), 18.65)
+
+    def test_price_under_10_10_cash_20_discount(self):
+            self.assertEquals(calculate_price(9, 5, 20), 18.65)
+
+class TestSetFour(unittest.TestCase):
+    def test_pass(self):
+        pass
+    def test_price_over_50_5_cash_10_discount(self):
+        self.assertEquals(calculate_price(51, 5, 10), 18.65)
+
+    def test_price_over_50_5_cash_15_discount(self):
+        self.assertEquals(calculate_price(51, 5, 15), 18.65)
+
+    def test_price_over_505_cash_20_discount(self):
+        self.assertEquals(calculate_price(51, 5, 20), 18.65)
+
+    def test_price_over_50_10_cash_10_discount(self):
+            self.assertEquals(calculate_price(51, 5, 10), 18.65)
+
+    def test_price_over_50_10_cash_15_discount(self):
+            self.assertEquals(calculate_price(51, 5, 15), 18.65)
+
+    def test_price_over_50_10_cash_20_discount(self):
+            self.assertEquals(calculate_price(51, 5, 20), 18.65)
+
 if __name__ == '__main__':
     unittest.main()
