f77f2073da40ec3bb75ca2fb02029e0ab483fdf0
Kcook5400 <kcook@dmacc.edu>
2020-09-21 18:16:05 -0500

    Module 4 Testing Scenarios

--- Module 4/average_scores.py
+++ Module 4/average_scores.py
@@ -11,7 +11,7 @@ History:
 
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
-    if score1 < 0:
+    if score1 < 0 or score2 < 0:
         raise ValueError
     return (score1 + score2 + score3)/NUMBER_TESTS
 
