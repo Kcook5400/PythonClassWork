adfe08fbdefdf92fa98f95270e2a75f5c3f1665c
Kcook5400 <kcook@dmacc.edu>
2020-09-24 11:48:08 -0500

    Module 5 while loops

--- Module 5/Input_while.py
+++ Module 5/Input_while.py
@@ -13,22 +13,23 @@ History:
 flag = False
 input_array = []
 inputs = (input("Enter a number from 1 to 100 or type 'Exit' to quit"))
-if str(inputs) == "Exit":
-    exit()
-while flag == False:
-    try:
-        if str(inputs) == "Exit":
-            exit()
-        if int(inputs) >= 1 or int(inputs) <= 100:
-            input_array.append(inputs)
-        if int(inputs) < 1 or int(inputs) > 100:
-            print("That is not between 1 and 100")
-        inputs = input("Enter another number or type 'Exit' to quit")
-    except ValueError:
-        Catch = input("That is not an integer, do you want to quit the program?")
-        if Catch == "Yes":
-            flag = True
-        else:
+
+while str(inputs) != "Exit":
+
+    while flag == False:
+        try:
+            if str(inputs) == "Exit":
+                break
+            if int(inputs) >= 1 or int(inputs) <= 100:
+                input_array.append(inputs)
+            if int(inputs) < 1 or int(inputs) > 100:
+                print("That is not between 1 and 100")
             inputs = input("Enter another number or type 'Exit' to quit")
+        except ValueError:
+            Catch = input("That is not an integer, do you want to quit the program?")
+            if Catch == "Yes":
+                flag = True
+            inputs = input("Enter another number or type 'Exit' to quit")
+
 for x in input_array:
     print(x)
