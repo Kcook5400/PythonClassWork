fed5bd09bab5b865a16f43986002e49759d0ebe1
Kcook5400 <kcook@dmacc.edu>
2020-10-04 15:10:03 -0500

    Module 6 string functions testing

---
+++ Module 5/function_parameter_cook.py
@@ -0,0 +1,21 @@
+"""
+PEP: 8
+Title: function parameter
+Author: Kevin Cook
+Status: Active
+Type: Process
+Created: 23-September-2020
+Post: 23-September-2020
+History:
+"""
+
+def multiply_string(message, n):
+
+    for x in range(int(n)):
+        print (message)
+
+if __name__ == "__main__":
+    message = input("Enter your favorite word")
+    n = input("Enter a number")
+
+    multiply_string(message, n)
---
+++ Module 5/string_functions.py
@@ -0,0 +1,12 @@
+import unittest
+from function_parameter_cook import multiply_string
+
+class MyTestCase(unittest.TestCase):
+    def test_pass(self):
+        pass
+    def test_multiplyString(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
