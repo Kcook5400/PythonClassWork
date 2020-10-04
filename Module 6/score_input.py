9f60646173b0428967a75405ef4457cefd5492d7
Kcook5400 <kcook@dmacc.edu>
2020-10-04 18:11:12 -0500

    Module 6 string functions testing name validation

--- Module 6/score_input.py
+++ Module 6/score_input.py
@@ -1,9 +1,12 @@
 
 
 
-def score_input(test_name, test_score=0, retries =2, reminder='Please try again'):
-
-        testing_name = input(test_name)
-
-        return testing_name
-
+def score_input(test_name, test_score=0, reminder='Invalid test score, please try again'):
+    test_name
+    test_score
+    return test_name,test_score
+    while True:
+        ok_range = int(input(test_score))
+        if int(ok_range) in range(1, 100):
+            return test_name, test_score
+        print(reminder)
