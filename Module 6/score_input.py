78ec3525d4e44cc17a166ce0c1ec80357fa964c4
Kcook5400 <kcook@dmacc.edu>
2020-10-04 15:36:41 -0500

    Module 6 string functions testing

--- Module 6/score_input.py
+++ Module 6/score_input.py
@@ -1,7 +1,12 @@
-test_name = ""
-test_score = ""
-invalid_message = ""
+
 
 
 def score_input(test_name, test_score):
-    return "test"
+    test_name = input("Enter your test name:")
+    test_score = input("Enter your test score:")
+    invalid_message = "That is not in a valid range, please try again"
+    if int(test_score) not in range(0, 100):
+        return invalid_message
+    return test_name, test_score
+
+
--- Module 6/testing_score_input.py
+++ Module 6/testing_score_input.py
@@ -4,7 +4,7 @@ from score_input import score_input
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(score_input("test", "test"), "test")
+        self.assertEqual(score_input("test", 80), ("test", "80"))
 
 if __name__ == '__main__':
     unittest.main()
