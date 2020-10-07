89c5c3414f0caeca340091593244ed1503ce4548
Kcook5400 <kcook@dmacc.edu>
2020-10-06 19:31:07 -0500

    Module 7 fail basic list test

--- Module 7/basic_list.py
+++ Module 7/basic_list.py
@@ -1,4 +1,14 @@
 def make_list():
-    pass
-def get_input():
-    pass
+    i=0
+    input_return_list = []
+    while(i<3):
+        input_return_list.append(int((get_input(input = input("Enter a number")))))
+        i+=1
+    return input_return_list
+def get_input(input):
+    try:
+        input_check = int(input)
+        return input_check
+    except ValueError:
+        return "That is not a number"
+
--- Module 7/basic_list_test.py
+++ Module 7/basic_list_test.py
@@ -1,9 +1,10 @@
 import unittest
-from basic_list import make_list, get_list
+from unittest.mock import patch
+from basic_list import make_list, get_input
 
 
 class MyTestCase(unittest.TestCase):
-    @patch('get_input', return_value='5')
+    @patch('basic_list.get_input', return_value='5')
     def test_make_list(self, input):
         self.assertEqual(make_list(), [5, 5, 5])
 
