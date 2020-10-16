21a400ad28fb29dc930c0eb8737d58a4309dfc5c
Kcook5400 <kcook@dmacc.edu>
2020-10-15 20:35:21 -0500

    Module 8 date_time

--- Module 8/half_birthday.py
+++ Module 8/half_birthday.py
@@ -1,3 +1,11 @@
+
+from datetime import datetime, timedelta
+from time import strftime
+
+
 def half_birthday(birthday):
-    pass
+    six_months = datetime.strptime(str(birthday), '%m/%d/%Y')
+    half_birthday = six_months + timedelta(6*30)
+
+    return half_birthday.strftime('%m/%d/%Y')
 
--- Module 8/test_half_birthday.py
+++ Module 8/test_half_birthday.py
@@ -8,8 +8,8 @@ from half_birthday import half_birthday
 
 class MyTestCase(unittest.TestCase):
     def test_set_true(self):
-        mybday=6/25/1988
-        self.assertEqual(half_birthday(mybday), 12/25/1988)
+        mybday="06/25/1988"
+        self.assertEqual(half_birthday(mybday), "12/22/1988")
 
 
 if __name__ == '__main__':
