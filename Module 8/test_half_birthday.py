5e9f5d6758cd143d4a0af24657867ab192416f25
Kcook5400 <kcook@dmacc.edu>
2020-10-15 20:07:33 -0500

    Module 8 date_time

--- Module 8/test_half_birthday.py
+++ Module 8/test_half_birthday.py
@@ -8,7 +8,8 @@ from half_birthday import half_birthday
 
 class MyTestCase(unittest.TestCase):
     def test_set_true(self):
-        self.assertEqual(half_birthday(6/25/1988), 12/25/1988)
+        mybday=6/25/1988
+        self.assertEqual(half_birthday(mybday), 12/25/1988)
 
 
 if __name__ == '__main__':
