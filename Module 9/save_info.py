88061db9b0f29ea78e496914e14e5721b2976fea
Kcook5400 <kcook@dmacc.edu>
2020-10-27 13:40:31 -0500

    Module 9 work

--- Module 9/save_info.py
+++ Module 9/save_info.py
@@ -8,20 +8,25 @@ def save_info(Edate,Etime,Eam,Epm):
     if Eam:
         time_sec = "AM"
     else:
-        time_sec="PM"
+        time_sec = "PM"
 cal = calendar.Calendar()
 i=1
 year_list = []
+booked_dict = []
 booking = "Rocky"
-c = datetime.datetime(2020, 12, 31)
-c= c.strftime('%Y-%m-%d')
+c = datetime.datetime(2020, 12, 10)
+c = c.strftime('%Y-%m-%d')
 for i in range(1, 13):
     for d in [x for x in cal.itermonthdates(2020, i) if x.month == i]:
-        year_list.append(d)
+        year_list.append(str(d))
+
+if str(c) in year_list:
+    year_list.remove(c)
+    booked_dict.append(c + " " + booking)
 
 
-for b in range(1, len(year_list)):
-    if c == str(year_list[b]):
-        year_list.remove(year_list[b])
+
 for b in range(1, len(year_list)):
     print(year_list[b])
+print(booked_dict)
+
